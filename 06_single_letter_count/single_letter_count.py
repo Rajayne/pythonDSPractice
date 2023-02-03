4a5b421f400d48308f6329ae65b4b53cb8212a86
Rajayne <ryann.j.nealey@gmail.com>
2023-02-03 14:20:46 -0800

    Doctest Single Letter Count, convert to lowercase for check

--- 06_single_letter_count/single_letter_count.py
+++ 06_single_letter_count/single_letter_count.py
@@ -10,3 +10,10 @@ def single_letter_count(word, letter):
         >>> single_letter_count("Hello World", 'l')
         3
     """
+    count = 0
+    for char in word:
+        if char.lower() == letter.lower():
+            count += 1
+        else:
+            continue
+    return count
