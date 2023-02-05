69f0083e7686c05418986cc4a2dfdfc1177470b3
Rajayne <ryann.j.nealey@gmail.com>
2023-02-05 15:35:30 -0800

    Create c variable to count outside of update tuple

--- 07_multiple_letter_count/multiple_letter_count.py
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -7,3 +7,12 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    dic = {}
+    for char in phrase:
+        if char in dic:
+            c = int(dic[char])
+            c += 1
+            dic.update({char:c})
+        else:
+            dic.update({char: 1})
+    return dic
