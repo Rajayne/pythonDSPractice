b3e256726fb5507a1a77e633fed1a56aa110b0b6
Rajayne <ryann.j.nealey@gmail.com>
2023-02-06 09:15:36 -0800

    Used similar base code as multiple letter count but looped through values in key-value pairs to find most frequent num... Returned list of most frequent values initially but doctest only wanted single most-frequent value, commented additional code out

--- 17_mode/mode.py
+++ 17_mode/mode.py
@@ -11,3 +11,16 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+    frequency = {}
+    for num in nums:
+        frequency.setdefault(num, 0)
+        frequency[num] += 1
+    most_frequent = max(frequency.values())
+    # most_list = []
+    for num, freq in frequency.items():
+        if freq == most_frequent:
+            # most_list.append(num)
+            return num
+    # return most_list
+mode([1, 2, 1])
+mode([2, 2, 3, 3, 2])
