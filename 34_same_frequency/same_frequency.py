87bf59c1f18ed66eff0d9e544d17629635cc8b5e
Rajayne <ryann.j.nealey@gmail.com>
2023-02-07 10:17:33 -0800

    Int not iteratable, convert numbers to string and spread to dictionary with 0 values.  Counter loop through two dictionaries then compare

--- 34_same_frequency/same_frequency.py
+++ 34_same_frequency/same_frequency.py
@@ -10,3 +10,26 @@ def same_frequency(num1, num2):
         >>> same_frequency(1212, 2211)
         True
     """
+    num1 = str(num1)
+    count = dict.fromkeys(num1, 0)
+
+    num2 = str(num2)
+    count2 = dict.fromkeys(num2, 0)
+
+    for num in num1:
+        if num in count:
+            c = count[num]
+            c += 1
+            count.update({num: c})
+    for num in num2:
+        if num in count2:
+            c = count2[num]
+            c += 1
+            count2.update({num: c})
+    if count == count2:
+        print(True)
+    else:
+        print(False)
+
+same_frequency(551122, 221515)
+
